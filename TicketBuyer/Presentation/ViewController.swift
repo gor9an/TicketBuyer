b6a372d4f51a677f50854cabf770d73124beb27d
gor9an <gordanandrey@yandex.ru>
2023-10-28 19:51:34 +0300

    add some Main.storyboard items settings to func viewDidLoad

--- TicketBuyer/Presentation/ViewController.swift
+++ TicketBuyer/Presentation/ViewController.swift
@@ -9,20 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var actualView: UIView!
-    @IBOutlet weak var actualStackView: UIStackView!
-    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var generalImage: UIImageView!
+    //Main Stack View Images
+    @IBOutlet weak var firstMainStackViewImage: UIImageView!
+    @IBOutlet weak var secondMainStackViewImage: UIImageView!
+    @IBOutlet weak var thirdMainStackViewImage: UIImageView!
+    
+    @IBOutlet weak var forAllTimeScrollView: UIScrollView!
     @IBOutlet weak var actualScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        actualStackView.layer.cornerRadius = 20
-//        
-//        actualScrollView.layer.masksToBounds = true // даём разрешение на рисование рамки
-//        actualScrollView.layer.borderWidth = 4 // толщина рамки
-//        actualScrollView.layer.cornerRadius = 20 // радиус скругления углов рамки
-//        
-//        actualScrollView.layer.borderColor = UIColor.black.cgColor
+
+        //Scroll View settings
+        actualScrollView.layer.masksToBounds = true // даём разрешение на рисование рамки
+
+        actualScrollView.layer.cornerRadius = 30 // радиус скругления углов рамки
+        actualScrollView.layer.borderColor = UIColor.black.cgColor
+        actualScrollView.layer.borderWidth = 4 // толщина рамки
+        
+        forAllTimeScrollView.layer.masksToBounds = true
+        
+        forAllTimeScrollView.layer.cornerRadius = 30 // радиус скругления углов рамки
+        forAllTimeScrollView.layer.borderColor = UIColor.black.cgColor
+        forAllTimeScrollView.layer.borderWidth = 4 // толщина рамки
+        
+        //Images settings
+        firstMainStackViewImage.layer.cornerRadius = 30
+        secondMainStackViewImage.layer.cornerRadius = 30
+        thirdMainStackViewImage.layer.cornerRadius = 30
+        
+        generalImage.layer.cornerRadius = 50
+
 
     }
 
