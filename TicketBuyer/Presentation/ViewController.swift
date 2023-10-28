0ac1ddfc8fc240a54c14c6d972e9b586b012f5bf
gor9an <gordanandrey@yandex.ru>
2023-10-28 20:40:04 +0300

    some fix

--- TicketBuyer/Presentation/ViewController.swift
+++ TicketBuyer/Presentation/ViewController.swift
@@ -15,33 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondMainStackViewImage: UIImageView!
     @IBOutlet weak var thirdMainStackViewImage: UIImageView!
     
+    @IBOutlet weak var firstSecScrollVImage: UIImageView!
+    @IBOutlet weak var secondSecScrollVImage: UIImageView!
+    @IBOutlet weak var thirdSecScrollVImage: UIImageView!
+    
     @IBOutlet weak var forAllTimeScrollView: UIScrollView!
     @IBOutlet weak var actualScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
         //Scroll View settings
-        actualScrollView.layer.masksToBounds = true // даём разрешение на рисование рамки
-
-        actualScrollView.layer.cornerRadius = 30 // радиус скругления углов рамки
-        actualScrollView.layer.borderColor = UIColor.black.cgColor
-        actualScrollView.layer.borderWidth = 4 // толщина рамки
+        setScrollViewSettings(scrollView: forAllTimeScrollView)
+        setScrollViewSettings(scrollView: actualScrollView)
         
-        forAllTimeScrollView.layer.masksToBounds = true
-        
-        forAllTimeScrollView.layer.cornerRadius = 30 // радиус скругления углов рамки
-        forAllTimeScrollView.layer.borderColor = UIColor.black.cgColor
-        forAllTimeScrollView.layer.borderWidth = 4 // толщина рамки
         
         //Images settings
         firstMainStackViewImage.layer.cornerRadius = 30
         secondMainStackViewImage.layer.cornerRadius = 30
         thirdMainStackViewImage.layer.cornerRadius = 30
         
-        generalImage.layer.cornerRadius = 50
+        generalImage.layer.cornerRadius = 30
+        
+        firstSecScrollVImage.layer.cornerRadius = 30
+        secondSecScrollVImage.layer.cornerRadius = 30
+        thirdSecScrollVImage.layer.cornerRadius = 30
 
+    }
+    func setScrollViewSettings(scrollView: UIScrollView) {
+        scrollView.layer.masksToBounds = true // даём разрешение на рисование рамки
 
+        scrollView.layer.cornerRadius = 30 // радиус скругления углов рамки
+        scrollView.layer.borderColor = UIColor.black.cgColor
+        scrollView.layer.borderWidth = 4 // толщина рамки
     }
 
 
