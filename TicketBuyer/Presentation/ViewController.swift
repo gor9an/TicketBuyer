fd7d67ee78d92a6f09cc25ad1eaa5d831e84652c
gor9an <gordanandrey@yandex.ru>
2023-10-28 18:14:16 +0300

    create the application base

---
+++ TicketBuyer/Presentation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TicketBuyer
+//
+//  Created by Andrey Gordienko on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var actualView: UIView!
+    @IBOutlet weak var actualStackView: UIStackView!
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var actualScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        actualStackView.layer.cornerRadius = 20
+//        
+//        actualScrollView.layer.masksToBounds = true // даём разрешение на рисование рамки
+//        actualScrollView.layer.borderWidth = 4 // толщина рамки
+//        actualScrollView.layer.cornerRadius = 20 // радиус скругления углов рамки
+//        
+//        actualScrollView.layer.borderColor = UIColor.black.cgColor
+
+    }
+
+
+}
+
