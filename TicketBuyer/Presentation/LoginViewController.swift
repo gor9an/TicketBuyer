15fce5257e0353d1f38f95702f5d1a8390e9edac
gor9an <gordanandrey@yandex.ru>
2023-11-01 01:46:29 +0300

    try to add second view

--- TicketBuyer/Presentation/LoginViewController.swift
+++ TicketBuyer/Presentation/LoginViewController.swift
@@ -7,23 +7,57 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, AlertPresenterDelegate {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    private var usersGetSet: User = User()
+    private var alertPresenter: AlertPresenterProtocol = AlertPresenter()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        alertPresenter.delegate = self
+        loginButton.layer.cornerRadius = 15
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private function
+    @IBAction func loginButtonTap(_ sender: Any) {
+        for user in usersGetSet.users {
+            if (user.username == loginTextField.text)
+                && (user.password == passwordTextField.text) {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let viewController = storyboard.instantiateViewController(identifier: "MainViewController")
+                
+                let viewModel = AlertModel(
+                    title: "Успех",
+                    message: "Вы вошли под администратором",
+                    buttonText: "Продожить")
+                
+                alertPresenter.requestAlert(result: viewModel)
+                
+                loginTextField.isEnabled = false
+                passwordTextField.isEnabled = false
+                
+                self.present(viewController, animated: true, completion: nil)
+                break
+                 
+            } else {
+                let viewModel = AlertModel(
+                    title: "Неудача",
+                    message: "Вы ввели неправильный логин или пароль",
+                    buttonText: "Продожить")
+                alertPresenter.requestAlert(result: viewModel)
+                break
+                
+            }
+        }
     }
-    */
-
+    
+    func didReceiveAlert() {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
 }
--- TicketBuyer/Presentation/ViewController.swift
+++ TicketBuyer/Presentation/ViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 
 class ViewController: UIViewController, AlertPresenterDelegate {
     
-    @IBOutlet weak var loginStackView: UIStackView!
-    
-    @IBOutlet weak var mainStackView: UIStackView!
     @IBOutlet weak var generalImage: UIImageView!
     
     //Main Stack View Images
@@ -23,15 +20,13 @@ class ViewController: UIViewController, AlertPresenterDelegate {
     @IBOutlet weak var secondSecScrollVImage: UIImageView!
     @IBOutlet weak var thirdSecScrollVImage: UIImageView!
     
-    @IBOutlet weak var loginButton: UIButton!
+    
     @IBOutlet weak var logoutButton: UIButton!
     
     @IBOutlet weak var forAllTimeScrollView: UIScrollView!
     @IBOutlet weak var actualScrollView: UIScrollView!
 
 
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
     
     private var usersGetSet: User = User()
     private var alertPresenter: AlertPresenterProtocol = AlertPresenter()
@@ -58,41 +53,12 @@ class ViewController: UIViewController, AlertPresenterDelegate {
         secondSecScrollVImage.layer.cornerRadius = 30
         thirdSecScrollVImage.layer.cornerRadius = 30
         
-        loginButton.layer.cornerRadius = 15
         logoutButton.layer.cornerRadius = 15
-        mainStackView.isHidden = true
+
 
     }
     
     // MARK: - Private functions
-    @IBAction private func loginButtonClick(_ sender: Any) {
-        for user in usersGetSet.users {
-            if (user.username == loginTextField.text)
-                && (user.password == passwordTextField.text) {
-                let viewModel = AlertModel(
-                    title: "Успех",
-                    message: "Вы вошли под администратором",
-                    buttonText: "Продожить")
-                
-                alertPresenter.requestAlert(result: viewModel)
-                
-                loginStackView.isHidden = true
-                mainStackView.isHidden = false
-
-                loginTextField.isEnabled = false
-                passwordTextField.isEnabled = false
-                
-            } else {
-                let viewModel = AlertModel(
-                    title: "Неудача",
-                    message: "Вы ввели неправильный логин или пароль",
-                    buttonText: "Продожить")
-                alertPresenter.requestAlert(result: viewModel)
-            }
-            
-        }
-    }
-    
     @IBAction func logoutButtonClick(_ sender: Any) {
         let viewModel = AlertModel(
             title: "Успех",
@@ -101,17 +67,12 @@ class ViewController: UIViewController, AlertPresenterDelegate {
         
         alertPresenter.requestAlert(result: viewModel)
         
-        loginStackView.isHidden = false
-        mainStackView.isHidden = true
+        self.dismiss(animated: true, completion: nil)
         
-        loginTextField.isEnabled = true
-        passwordTextField.isEnabled = true
     }
     
     // MARK: - AlertPresenterDelegate
-    func didReceiveAlert() { 
-        loginTextField.text = ""
-        passwordTextField.text = ""
+    func didReceiveAlert() {
     }
     
 }
